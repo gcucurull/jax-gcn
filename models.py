4cf9fc7b49c35eb73c459d4a15c0769f386ee132
Guillem Cucurull <gcucurull@gmail.com>
2020-04-06 22:06:31 +0100

    initial code

---
+++ models.py
@@ -0,0 +1,69 @@
+import jax.numpy as np
+from jax.nn.initializers import glorot_normal, normal, ones, zeros
+
+
+class GraphConvolutionPytorch(Module):
+    """
+    Simple GCN layer, similar to https://arxiv.org/abs/1609.02907
+    """
+
+    def __init__(self, in_features, out_features, bias=True):
+        super(GraphConvolution, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = Parameter(torch.FloatTensor(in_features, out_features))
+        if bias:
+            self.bias = Parameter(torch.FloatTensor(out_features))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.weight.size(1))
+        self.weight.data.uniform_(-stdv, stdv)
+        if self.bias is not None:
+            self.bias.data.uniform_(-stdv, stdv)
+
+    def forward(self, input, adj):
+        support = torch.mm(input, self.weight)
+        output = torch.spmm(adj, support)
+        if self.bias is not None:
+            return output + self.bias
+        else:
+            return output
+
+    def __repr__(self):
+        return self.__class__.__name__ + ' (' \
+               + str(self.in_features) + ' -> ' \
+               + str(self.out_features) + ')'
+
+def GraphConvolution(out_dim, W_init=glorot_normal(), b_init=normal()):
+    """
+    Layer constructor function for a Graph Convolution layer similar to https://arxiv.org/abs/1609.02907
+    """
+    def init_fun(ring, input_shape):
+        output_shape = input_shape[:-1] + (out_dim,)
+        k1, k2 = random.split(rng)
+        W, b = W_init(k1, (input_shape[-1], out_dim)), b_init(k2, (out_dim,))
+        return output_shape, (W, b)
+    def apply_fun(params, inputs, **kwargs):
+        W, b = params
+        return np.dot(inputs, W) + b
+    return init_fun, apply_fun
+
+def GCN(nfeat, nhid, nclass, dropout):
+    pass
+
+class GCNPytorch(nn.Module):
+    def __init__(self, nfeat, nhid, nclass, dropout):
+        super(GCN, self).__init__()
+
+        self.gc1 = GraphConvolution(nfeat, nhid)
+        self.gc2 = GraphConvolution(nhid, nclass)
+        self.dropout = dropout
+
+    def forward(self, x, adj):
+        x = F.relu(self.gc1(x, adj))
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = self.gc2(x, adj)
+        return F.log_softmax(x, dim=1)
---
+++ train.py
@@ -0,0 +1,9 @@
+import argparse
+
+import jax.numpy as np
+
+def main():
+    pass
+
+def train():
+    pass
